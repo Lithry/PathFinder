e02846f855bbb3181a28ad292cf6474542d36f4e
Lithry <facundosargiotti@gmail.com>
2017-08-19 04:08:19 -0300

    Creación de grilla de nodos.

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node{
+    Node parent;
+    private List<Node> adyacents = new List<Node>();
+    private Vector3 pos;
+
+    public void AddAdyacent(Node ady) {
+        adyacents.Add(ady);
+    }
+
+    public List<Node> GetAdyacents(){
+        return adyacents;
+    }
+
+    public void SetParent(Node pNode) {
+        parent = pNode;
+    }
+
+    public Node GetParent(){
+        return parent;
+    }
+
+    public void SetPosition(Vector3 nPos) {
+        pos = nPos;
+    }
+
+    public Vector3 GetPosition(){
+        return pos;
+    }
+
+}
---
+++ Assets/Scripts/PathFinder.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinder : MonoBehaviour {
+	static public PathFinder instance;
+    private List<Node> nodes = new List<Node>();
+    private List<Node> openNodes = new List<Node>();
+    private List<Node> closedNodes = new List<Node>();
+    private List<Node> path = new List<Node>();
+    private Node startNode;
+    private Node endNode;
+
+    void Start () {
+        instance = this;
+
+        for (int x = -10; x < 10; x++){
+			for (int z = -10; z < 10; z++){
+				Node nNode = new Node();
+				nNode.SetPosition(new Vector3(x, 0, z));
+				nodes.Add(nNode);
+			}
+		}
+
+		for (int i = 0; i< nodes.Count; i++){
+			for (int j = 0; j< nodes.Count; j++){
+				if (nodes[i] != nodes[j]){
+					if (Vector3.Distance(nodes[i].GetPosition(), nodes[j].GetPosition()) <= 1){
+						nodes[i].AddAdyacent(nodes[j]);
+					}
+				}
+			}
+		}
+    }
+	
+    public void FindPhat(Vector3 beginPos, Vector3 endPos) {
+
+    }
+	
+    private void OpenNode(Node node) {
+
+    }
+#if   UNITY_EDITOR    
+	void OnDrawGizmos(){
+		Gizmos.color = Color.white;
+		for (int i = 0; i < nodes.Count; i++){
+			Gizmos.DrawSphere(nodes[i].GetPosition(), 0.25f);
+			for (int j = 0; j < nodes[i].GetAdyacents().Count; j++){
+				Gizmos.DrawLine(nodes[i].GetPosition(), nodes[i].GetAdyacents()[j].GetPosition());
+			}
+		}
+	}
+#endif
+}
